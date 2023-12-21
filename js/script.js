6104d6b229bfd53003297f7d58ea39777c3520d4
YesiAquino <yesiaquino95@gmail.com>
2023-12-21 11:13:17 -0300

    cambio habilidades

--- js/script.js
+++ js/script.js
@@ -22,16 +22,16 @@ function seleccionar(){
      var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
      if(distancia_skills >= 300){
          let habilidades = document.getElementsByClassName("progreso");
-         habilidades[0].classList.add("javascript");
-         habilidades[1].classList.add("htmlcss");
-         habilidades[2].classList.add("photoshop");
-         habilidades[3].classList.add("wordpress");
-         habilidades[4].classList.add("drupa");
+         habilidades[0].classList.add("html");
+         habilidades[1].classList.add("css");
+         habilidades[2].classList.add("javascript");
+         habilidades[3].classList.add("react");
+         habilidades[4].classList.add("php");
          habilidades[5].classList.add("comunicacion");
          habilidades[6].classList.add("trabajo");
          habilidades[7].classList.add("creatividad");
-         habilidades[8].classList.add("dedicacion");
-         habilidades[9].classList.add("proyect");
+         habilidades[8].classList.add("responsabilidad");
+         habilidades[9].classList.add("aprendizaje");
      }
  }
 

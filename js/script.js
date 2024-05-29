e435a4ecea6de4b9d588ab8a0f2e33de6f85a05b
YesiAquino <yesiaquino95@gmail.com>
2024-05-28 23:36:16 -0300

    Actualizaciones1

--- js/script.js
+++ js/script.js
@@ -16,7 +16,7 @@ function seleccionar(){
     menuVisible = false;
 }
 
-// funcion que aplica las animaciones de las habilidades
+// funcion de animaciones de las habilidades
     function efectoHabilidades(){
      var skills = document.getElementById("skills");
      var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
